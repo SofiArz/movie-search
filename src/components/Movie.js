275aa70b791757fad492291f1febe60853c1ecef
Sofia Arzamendia <sofia.arzamendia@hotmail.es>
2024-07-06 23:29:13 +0200

    scale animation on hover

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -3,27 +3,25 @@ import { Link } from "react-router-dom";
 export const Movie = ({ poster, title, year, id }) => {
 
     return (
-        <Link to={`movie/${id}`}>
-            <div className="card">
-                <div className="card-image">
-                    <figure className="image">
-                        <img
-                            src={poster}
-                            alt={title}
-                            style={{height:"440px"}}
-                        />
-                    </figure>
-                </div>
-                <div className="card-content hide">
-                    <div className="media" style={{height:"100%"}}>
-                        <div className="media-content" style={{ paddingBottom:"10px"}}>
-                            <p className="title is-4">{title}</p>
-                            <p className="subtitle is-6 ">{year}</p>
-                        </div>
+        <div className="card">
+            <div className="card-image">
+                <figure className="image">
+                    <img
+                        src={poster}
+                        alt={title}
+                        style={{ height: "440px" }}
+                    />
+                </figure>
+            </div>
+            <div className="card-content hide">
+                <div className="media">
+                    <div className="media-content" style={{ paddingBottom: "10px" }}>
+                        <p className="title is-4">{title}</p>
+                        <p className="subtitle is-6 ">{year}</p>
                     </div>
                 </div>
             </div>
-        </Link>
+        </div>
     );
 }
 
